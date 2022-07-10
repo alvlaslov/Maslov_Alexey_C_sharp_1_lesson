687f6a6d21068b573e71522372e99ecec4ca5650
Alexey Maslov <alvladigital@gmail.com>
2022-07-10 19:39:38 +0300

    Task4 add equality condition

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,39 +4,52 @@
 // First solution
 void FirstSolution()
 {
-Console.WriteLine ("Insert first number: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert second number; ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert third number: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-int max = number1;
+    Console.WriteLine ("Insert first number: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Insert second number: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Insert third number: ");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+    if(number1 == number2 && number1 == number3)
+    {
+        Console.WriteLine ("The numbers are equal");
+    }
+    else
+        {
+        int max = number1;
+        if (number2 > number1)
+            {
+                max = number2;
+            }
+        if (number3 > max)
+            {
+                max = number3;
+            }
 
-if (number2 > number1)
-{
-    max = number2;
-}
-if (number3 > max)
-{
-    max = number3;
-}
-
-Console.WriteLine($"max = {max}");
+            Console.WriteLine($"max = {max}");
+        }
 }
 
 // Second solution
 void SecondSolution()
 {
-Console.WriteLine ("Insert first number: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert second number; ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert third number: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-int max = Math.Max(number1, Math.Max(number2, number3));
+    Console.WriteLine ("Insert first number: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Insert second number; ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Insert third number: ");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+    if(number1 == number2 && number1 == number3)
+    {
+        Console.WriteLine ("The numbers are equal");
+    }
+    else
+    {
+        int max = Math.Max(number1, Math.Max(number2, number3));
 
-Console.WriteLine($"max = {max}");
+        Console.WriteLine($"max = {max}");
+    }
 }
 
-//FirstSolution();
-SecondSolution();
+FirstSolution();
+//SecondSolution();
